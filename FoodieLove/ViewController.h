5a12a6282631ef45009ccf790cbff58fe60d7f63
Sonova Middleton <supernova@atreyu.local>
2015-06-29 14:25:22 -0400

    Initial Commit

---
+++ FoodieLove/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FoodieLove
+//
+//  Created by Sonova Middleton on 6/29/15.
+//  Copyright (c) 2015 supernova8productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FoodieLove/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FoodieLove
+//
+//  Created by Sonova Middleton on 6/29/15.
+//  Copyright (c) 2015 supernova8productions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
